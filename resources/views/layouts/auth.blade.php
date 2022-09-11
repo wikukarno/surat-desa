a1defff9d3e4bd86e424d5d887b00640ecc58d4d
wikukarno <prasetyagama2@gmail.com>
2022-09-12 02:01:24 +0700

    update

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,10 +4,15 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>@yield('title')</title>
+  @stack('prepend-style')
   @include('includes.home.style')
+  @stack('after-style')
 </head>
 <body class="hold-transition login-page">
     @yield('content')
+
+    @stack('prepend-script')
     @include('includes.home.script')
+    @stack('addon-script')
 </body>
 </html>
