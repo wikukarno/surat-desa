1205968d417b8ef05cf1d8942d64b436d3c20aff
wikukarno <prasetyagama2@gmail.com>
2022-09-24 12:04:26 +0700

    update required

--- resources/views/pages/user/sku/modal-tambah-sku.blade.php
+++ resources/views/pages/user/sku/modal-tambah-sku.blade.php
@@ -18,21 +18,21 @@
                             <div class="form-group">
                                 <label for="nama">NIK</label>
                                 <input type="number" class="form-control" id="no_nik" name="no_nik"
-                                    placeholder="Masukkan nomor nik">
+                                    placeholder="Masukkan nomor nik" required>
                             </div>
                         </div>
                         <div class="col-4">
                             <div class="form-group">
                                 <label for="nama">Nama</label>
                                 <input type="text" class="form-control" id="nama" name="nama"
-                                    placeholder="Masukkan nama">
+                                    placeholder="Masukkan nama" required>
                             </div>
                         </div>
                         <div class="col-4">
                             <div class="form-group">
                                 <label for="nama">Nama Usaha</label>
                                 <input type="text" class="form-control" id="nama_usaha" name="nama_usaha"
-                                    placeholder="Masukkan nama usaha">
+                                    placeholder="Masukkan nama usaha" required>
                             </div>
                         </div>
                     </div>
@@ -42,14 +42,14 @@
                             <div class="form-group">
                                 <label for="nama">Tempat Lahir</label>
                                 <input type="text" class="form-control" id="tempat_lahir" name="tempat_lahir"
-                                    placeholder="Masukkan tempat lahir">
+                                    placeholder="Masukkan tempat lahir" required>
                             </div>
                         </div>
                         <div class="col-6">
                             <div class="form-group">
                                 <label for="tanggal_lahir">Tanggal Lahir</label>
                                 <input type="text" class="form-control" id="tanggal_lahir" name="tanggal_lahir"
-                                    placeholder="Masukkan tanggal lahir">
+                                    placeholder="Masukkan tanggal lahir" required>
                             </div>
                         </div>
                     </div>
@@ -58,7 +58,7 @@
                         <div class="col-4">
                             <div class="form-group">
                                 <label for="nama">Jenis Kelamin</label>
-                                <select name="jenis_kelamin" class="form-control" id="jenis_kelamin">
+                                <select name="jenis_kelamin" class="form-control" id="jenis_kelamin" required>
                                     <option value="Laki-laki">Laki-laki</option>
                                     <option value="Perempuan">Perempuan</option>
                                 </select>
@@ -68,14 +68,14 @@
                             <div class="form-group">
                                 <label for="nama">Pekerjaan</label>
                                 <input type="text" class="form-control" id="pekerjaan" name="pekerjaan"
-                                    placeholder="Masukkan pekerjaan">
+                                    placeholder="Masukkan pekerjaan" required>
                             </div>
                         </div>
                         <div class="col-4">
                             <div class="form-group">
                                 <label for="nama">Status Perkawinan</label>
                                 <input type="text" class="form-control" id="status_perkawinan" name="status_perkawinan"
-                                    placeholder="Masukkan status perkawinan">
+                                    placeholder="Masukkan status perkawinan" required>
                             </div>
                         </div>
                     </div>
@@ -84,7 +84,7 @@
                         <div class="col-4">
                             <div class="form-group">
                                 <label for="nama">Agama</label>
-                                <select class="form-control" id="agama" name="agama">
+                                <select class="form-control" id="agama" name="agama" required>
                                     <option value="Islam">Islam</option>
                                     <option value="Kristen">Kristen</option>
                                     <option value="Katolik">Katolik</option>
@@ -98,14 +98,14 @@
                             <div class="form-group">
                                 <label for="nama">Kecamatan</label>
                                 <input type="text" class="form-control" id="kecamatan" name="kecamatan"
-                                    placeholder="Masukkan kecamatan">
+                                    placeholder="Masukkan kecamatan" required>
                             </div>
                         </div>
                         <div class="col-4">
                             <div class="form-group">
                                 <label for="nama">Kelurahan</label>
                                 <input type="text" class="form-control" id="kelurahan" name="kelurahan"
-                                    placeholder="Masukkan kelurahan">
+                                    placeholder="Masukkan kelurahan" required>
                             </div>
                         </div>
                     </div>
@@ -115,14 +115,14 @@
                             <div class="form-group">
                                 <label for="nama">RT / RW</label>
                                 <input type="text" class="form-control" id="rt_rw" name="rt_rw"
-                                    placeholder="Masukkan RT/RW">
+                                    placeholder="Masukkan RT/RW" required>
                             </div>
                         </div>
                         <div class="col-6">
                             <div class="form-group">
                                 <label for="alamat">Alamat</label>
                                 <input type="text" class="form-control" id="alamat" name="alamat"
-                                    placeholder="Masukkan alamat">
+                                    placeholder="Masukkan alamat" required>
                             </div>
                         </div>
                     </div>
@@ -131,19 +131,20 @@
                         <div class="col-4">
                             <div class="form-group">
                                 <label for="">Foto KTP</label>
-                                <input type="file" class="form-control" id="foto_ktp" name="foto_ktp">
+                                <input type="file" class="form-control" id="foto_ktp" name="foto_ktp" required>
                             </div>
                         </div>
                         <div class="col-4">
                             <div class="form-group">
                                 <label for="">Foto KK</label>
-                                <input type="file" class="form-control" id="foto_kk" name="foto_kk">
+                                <input type="file" class="form-control" id="foto_kk" name="foto_kk" required>
                             </div>
                         </div>
                         <div class="col-4">
                             <div class="form-group">
                                 <label for="">Surat Keterangan RT/RW</label>
-                                <input type="file" class="form-control" id="foto_surat_rt_rw" name="foto_surat_rt_rw">
+                                <input type="file" class="form-control" id="foto_surat_rt_rw" name="foto_surat_rt_rw"
+                                    required>
                             </div>
                         </div>
                     </div>
